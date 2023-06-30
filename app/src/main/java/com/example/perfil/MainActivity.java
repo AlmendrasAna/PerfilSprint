df528b2ed2d5d67fc2d9e02c60835d1cfbe1dbc1
Ana <almendras451@gmail.com>
2023-06-29 21:01:35 -0400

    se mueve entre activity

--- app/src/main/java/com/example/perfil/MainActivity.java
+++ app/src/main/java/com/example/perfil/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.perfil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.perfil.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+binding.button.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent( MainActivity.this, MainActivity2.class );
+        startActivity(intent);
+    }
+});
+
     }
 }
