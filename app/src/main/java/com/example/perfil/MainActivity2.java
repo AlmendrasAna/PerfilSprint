a598dbdb84a3fe2b4b503bf3ca89417c2ed13f55
Ana <almendras451@gmail.com>
2023-06-29 21:06:22 -0400

    boton linkkedin

--- app/src/main/java/com/example/perfil/MainActivity2.java
+++ app/src/main/java/com/example/perfil/MainActivity2.java
@@ -2,13 +2,27 @@ package com.example.perfil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.perfil.databinding.ActivityMain2Binding;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    private ActivityMain2Binding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intertWeb = new Intent(Intent.ACTION_VIEW, Uri.parse("https://cl.linkedin.com/"));
+                startActivity(intertWeb);
+            }
+        });
     }
 }
